8f81be581b6ab57e5ae6369afe2445bdb3b3aa39
DmitrijProcevnikov <dmitrijprocevnikov@gmail.com>
2022-10-18 00:44:07 +0300

    test

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,24 +1,29 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-/**
- * Given matrix where you have to find cats by ears "^^"
- *
- * @param {Array<Array>} matrix 
- * @return {Number} count of cats found
- *
- * @example
- * countCats([
- *  [0, 1, '^^'],
- *  [0, '^^', 2],
- *  ['^^', 1, 2]
- * ]) => 3`
- *
- */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
 
+//   Given matrix where you have to find cats by ears "^^"
+ 
+//   @param {Array<Array>} matrix 
+//  @return {Number} count of cats found
+ 
+//   @example
+  // const m = [
+  //  [0, 1, '^^'],
+  //  [0, '^^', 2],
+  //  ['^^', 1, 2]
+  // ]
+ 
+ 
+function countCats(n) {
+  let sum = 0;
+  for(let i = 0; i < n.length; i++){
+    for(let j = 0; j < n[i].length; j++){
+      if(n[i][j] === '^^'){
+        sum += 1;
+      }
+    } 
+  }return sum;
+}
 module.exports = {
   countCats
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(n) {
+  let g = [];
+  let q = [];
+  let s = [];
+  if(n === null){
+    return false;
+  }
+  if(n === undefined){
+    return false;
+  }
+  for (let i = 0; i < n.length; i++){
+    if(typeof n[i] === 'string'){
+      g.push(n[i].trim());
+    } 
+  }
+  if(g.length === 0){
+    return false;
+  }
+  for(let t = 0; t < g.length; t++){
+    s.push(g[t][0].toUpperCase());
+   
+  }
+  q = s.sort();
+    
+
+  return q.join('');
 }
 
 module.exports = {
